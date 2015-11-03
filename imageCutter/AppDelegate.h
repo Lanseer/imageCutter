66725204854fc655f62bfaa40af8d474d96f96c3
lanseer <lanseer@foxmail.com>
2015-11-03 18:57:57 +0800

    imageCutter
    imageCutter

---
+++ imageCutter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageCutter
+//
+//  Created by Lanseer on 15/11/2.
+//  Copyright © 2015年 Lacar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
